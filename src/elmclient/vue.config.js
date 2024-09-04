288fda44bc02d3f4834a427369fe14d4c7e344d1
shirokanerinko0 <118335169+shirokanerinko0@users.noreply.github.com>
2024-09-04 17:09:41 +0800

    Add files via upload

--- src/elmclient/vue.config.js
+++ src/elmclient/vue.config.js
@@ -1,5 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 module.exports = {
-	devServer: {
-		port: 8081
-	}
+ devServer: {
+ port: 8081
+ }
 }
+
