3fc62f67c52d8235df33dcf8bc0a5e4858073768
madao2333 <18522374358@163.com>
2024-09-05 20:49:02 +0800

    修改了送货地址用户和电话显示错误的问题

--- src/elmclient/src/views/Orders.vue
+++ src/elmclient/src/views/Orders.vue
@@ -14,7 +14,7 @@
 				<p>{{deliveryaddress!=null?deliveryaddress.address:'请选择送货地址'}}</p>
 				<i class="fa fa-angle-right"></i>
 			</div>
-			<p>{{user.userName}}{{user.userSex | sexFilter}} {{user.userId}}</p>
+			<p>{{deliveryaddress.contactName}}{{deliveryaddress.contactSex | sexFilter}} {{deliveryaddress.contactTel}}</p>
 		</div>
 
 		<h3>{{business.businessName}}</h3>
