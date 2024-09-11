0e8c02e9cf888874a58a7f3f674114614287a230
Elizabethou <3144705667@qq.com>
2024-09-11 15:26:57 +0800

    美化了Foodinfo,但是接口任由问题，以及没有做安全性验证

--- src/elmclient/src/views/FoodInfo.vue
+++ src/elmclient/src/views/FoodInfo.vue
@@ -9,38 +9,39 @@
 
 		<!-- 食品logo部分 -->
 		<div class="business-logo">
-			<img :src="business.businessImg">
+			<img :src="business.businessImg" alt="Business Logo">
 		</div>
+
 		<!-- 食品信息部分 -->
-		<div>
-			<div>
-				图片<input type="text" v-model="food.foodImg" style="font-size: 4vw; width: 20vw;height: 4vw;">{{
-					food.foodImg }}</input>
+		<div class="food-info-container">
+			<div class="food-input-group">
+				<label>图片</label>
+				<input type="text" v-model="food.foodImg">
 			</div>
-			<div>
-				名称<input type="text" v-model="food.foodName" style="font-size: 4vw; width: 20vw;height: 4vw;">{{
-					food.foodName }}</input>
+			<div class="food-input-group">
+				<label>名称</label>
+				<input type="text" v-model="food.foodName">
 			</div>
-			<div>
-				价格<input type="text" v-model="food.foodPrice" style="font-size: 4vw; width: 20vw;height: 4vw;">{{
-					food.foodPrice }}</input>
+			<div class="food-input-group">
+				<label>价格</label>
+				<input type="text" v-model="food.foodPrice">
 			</div>
-			<div>
-				简介<input type="text" v-model="food.foodExplain" style="font-size: 4vw; width: 50vw;height: 20vw;">{{
-					food.foodExplain }}</input>
+			<div class="food-input-group">
+				<label>简介</label>
+				<textarea v-model="food.foodExplain"></textarea>
 			</div>
-			<div>
-				限量<input type="text" v-model="food.quantity" style="font-size: 4vw; width: 50vw;height: 20vw;">{{
-					food.quantity }}</input>
+			<div class="food-input-group">
+				<label>限量</label>
+				<input type="text" v-model="food.quantity">
 			</div>
-			<div>
-				<button @click="submitChanges" class="addButton" style="font-size: 4vw;">保存更改</button>
+			<div class="button-group">
+				<button @click="submitChanges" class="save-button">保存更改</button>
+				<button @click="removeFood" class="delete-button">移除食品</button>
 			</div>
 		</div>
-		<div>
-			<button @click="removeFood" class="addButton">移除食品</button>
-		</div>
-		<div class="blank" style="height: 17vw"></div>
+
+		<!-- 底部空白和Footer -->
+		<div class="blank"></div>
 		<Footer></Footer>
 	</div>
 </template>
@@ -48,6 +49,7 @@
 <script>
 import Footer from '../components/Footer.vue';
 import BackButton from '../components/BackButton.vue';
+
 export default {
 	name: 'FoodInfo',
 	data() {
@@ -61,7 +63,7 @@ export default {
 	created() {
 		this.user = this.$getSessionStorage('user');
 		this.business = this.$getSessionStorage('business');
-		//根据foodId查询食品信息
+		// 根据foodId查询食品信息
 		this.$axios.post('Food', this.$qs.stringify({
 			foodId: this.foodId
 		})).then(response => {
@@ -87,26 +89,25 @@ export default {
 					}).catch(error => {
 						console.error(error);
 					});
+				} else {
+					alert("更新失败");
 				}
-				else alert("更新失败");
-			})
-				.catch(error => {
-					console.error('失败:', error);
-				});
+			}).catch(error => {
+				console.error('失败:', error);
+			});
 		},
-		removeFood(){
+		removeFood() {
 			this.$axios.post('/Food', this.$qs.stringify({
-			foodId: this.foodId
-		})).then(response => {
-			if (response.data > 1){
-				alert("更新成功");
-			}
-		}).catch(error => {
-			console.error(error);
-		});
+				foodId: this.foodId
+			})).then(response => {
+				if (response.data > 1) {
+					alert("更新成功");
+				}
+			}).catch(error => {
+				console.error(error);
+			});
 		}
-
-	},
+	}
 }
 </script>
 
@@ -114,7 +115,11 @@ export default {
 /****************** 总容器 ******************/
 .wrapper {
 	width: 100%;
-	height: 100%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background-color: #f9f9f9;
 }
 
 /****************** header部分 ******************/
@@ -122,117 +127,100 @@ export default {
 	width: 100%;
 	height: 12vw;
 	background-color: #0097FF;
-	color: #fff;
+	color: white;
 	font-size: 4.8vw;
-
 	position: fixed;
-	left: 0;
 	top: 0;
+	left: 0;
 	z-index: 1000;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
+/****************** 食品logo部分 ******************/
+.business-logo {
+	width: 100%;
+	height: 40vw;
+	margin-top: 12vw; /* 留出header的空间 */
 	display: flex;
 	justify-content: center;
 	align-items: center;
 }
 
-/****************** 食品列表部分 ******************/
-.wrapper .food {
-	width: 100%;
-	/*使用下外边距避开footer部分*/
-	margin-bottom: 14vw;
-}
-
-.wrapper .food li {
-	width: 100%;
-	box-sizing: border-box;
-	padding: 2.5vw;
-	user-select: none;
-
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-}
-
-.wrapper .food li .food-left {
-	display: flex;
-	align-items: center;
-}
-
-.wrapper .food li .food-left img {
-	width: 20vw;
-	height: 20vw;
-}
-
-.wrapper .food li .food-left .food-left-info {
-	margin-left: 3vw;
-}
-
-.wrapper .food li .food-left .food-left-info h3 {
-	font-size: 3.8vw;
-	color: #555;
-}
-
-.wrapper .food li .food-left .food-left-info p {
-	font-size: 3vw;
-	color: #888;
-	margin-top: 2vw;
-}
-
-.wrapper .food li .food-right {
-	width: 16vw;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-}
-
-.wrapper .food li .food-right .fa-minus-circle {
-	font-size: 5.5vw;
-	color: #999;
-	cursor: pointer;
-}
-
-.wrapper .food li .food-right p {
-	font-size: 3.6vw;
-	color: #333;
-}
-
-.wrapper .food li .food-right .fa-plus-circle {
-	font-size: 5.5vw;
-	color: #0097EF;
-	cursor: pointer;
-}
-
-.wrapper .business-logo {
-	width: 100%;
-	height: 35vw;
-	/*使用上外边距避开header部分*/
-	margin-top: 12vw;
-
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-
-.wrapper .business-logo img {
+.business-logo img {
 	width: 40vw;
-	height: 30vw;
-	border-radius: 5px;
+	height: auto;
+	border-radius: 10px;
+	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
-.wrapper .addButton {
+/****************** 食品信息部分 ******************/
+.food-info-container {
+	padding: 4vw;
+	margin-top: 4vw;
+}
+
+.food-input-group {
+	margin-bottom: 4vw;
+}
+
+.food-input-group label {
 	font-size: 4vw;
+	color: #333;
+	display: block;
+	margin-bottom: 2vw;
+}
+
+.food-input-group input, .food-input-group textarea {
+	width: 100%;
+	font-size: 4vw;
+	padding: 2vw;
+	border: 1px solid #ddd;
+	border-radius: 5px;
+	box-sizing: border-box;
+}
+
+.food-input-group textarea {
+	height: 20vw;
+	resize: none;
+}
+
+.button-group {
 	display: flex;
-	justify-content: center;
-	/* 水平居中 */
-	align-items: center;
-	/* 垂直居中 */
-	padding: 10px 20px;
-	/* 按钮内边距 */
-	margin-top: 2vw;
-	height: 10vw;
-	margin-left: auto;
-	margin-right: auto;
-	width: fit-content;
-	/* 按钮宽度根据内容自适应 */
+	justify-content: space-between;
+	margin-top: 6vw;
+}
+
+.save-button {
+	background-color: #0097FF;
+	color: white;
+	border: none;
+	border-radius: 5px;
+	padding: 3vw 6vw;
+	font-size: 4vw;
+	cursor: pointer;
+}
+
+.delete-button {
+	background-color: #FF4C4C;
+	color: white;
+	border: none;
+	border-radius: 5px;
+	padding: 3vw 6vw;
+	font-size: 4vw;
+	cursor: pointer;
+}
+
+.blank {
+	height: 17vw;
+}
+
+/****************** 底部Footer ******************/
+.footer {
+	position: fixed;
+	bottom: 0;
+	width: 100%;
 }
 </style>
