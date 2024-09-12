d6d17b20bbe020c26bf0548274f952faa87108cb
Elizabethou <3144705667@qq.com>
2024-09-12 19:38:52 +0800

    可以更改商家名

--- src/elmboot/src/main/java/com/neusoft/elmboot/mapper/BusinessMapper.java
+++ src/elmboot/src/main/java/com/neusoft/elmboot/mapper/BusinessMapper.java
@@ -14,22 +14,22 @@ public interface BusinessMapper {
 
 	@Select("select * from business where orderTypeId=#{orderTypeId} order by businessId")
 	public List<Business> listBusinessByOrderTypeId(Integer orderTypeId);
-	
+
 	@Select("select * from business where businessId=#{businessId}")
 	public Business getBusinessById(Integer businessId);
-	
+
 	@Select("select * from business where businessName like CONCAT('%', #{keyWord}, '%')")
 	public List<Business> listBusinessByKeyWord(String keyWord);
-	
+
 	@Insert("insert into business (businessName) values (#{businessName})")
 	public void saveBusiness(String businessName);
-	
+
 	@Select("select count(*) from business")
 	public Integer getBusinessesNumber();
-	
-	@Update("update business set businessAddress = #{businessAddress}, businessExplain = #{businessExplain}, orderTypeId = #{orderTypeId}, starPrice = #{starPrice}, deliveryPrice = #{deliveryPrice} where businessId = #{businessId}")
+
+	@Update("update business set businessName = #{businessName}, businessAddress = #{businessAddress}, businessExplain = #{businessExplain}, orderTypeId = #{orderTypeId}, starPrice = #{starPrice}, deliveryPrice = #{deliveryPrice} where businessId = #{businessId}")
 	public int updateBusiness(Business business);
-	
+
 	@Update("update business set businessImg = #{businessImg} where businessId = #{businessId}")
 	public int updateBusinessImg(String businessImg, String businessId);
 }
